aee3c75b54e1ae4bcebcbce170644e4993ed1749
Def <deferenseo@gmail.com>
2023-05-25 15:41:43 +0300

    setted routes in Dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
 
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+
+                <div className={`${classes.dialog} ${classes.active}`}>
+                    <NavLink to="/dialogs/1">Dmitriy</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/2">Andrey</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/3">Alex</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/4">Sarah</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/5">Galtran</NavLink>
+                </div>
+            </div>
+
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>some text</div>
+            </div>
         </div>
     )
 }
